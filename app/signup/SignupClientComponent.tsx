cefb39723bf7158a03e7b26e1afa0457838316b0
bosco0808 <bosco0808@gmail.com>
2025-04-04 16:50:12 +0800

    Finished frontend logic of signup page

--- app/signup/SignupClientComponent.tsx
+++ app/signup/SignupClientComponent.tsx
@@ -1,18 +1,30 @@
 "use client";
 
+import { useState } from "react";
+
 export default function SignUpClient() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <form>
       <h2 className="m-2">Sign Up</h2>
       <label className="m-2">Email address</label>
-      <input className="form-control m-2"></input>
+      <input
+        className="form-control m-2"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      ></input>
       <small className="form-text text-muted m-2">
         We'll never share your email with anyone else.
       </small>
       <br />
       <br />
       <label className="m-2">Password</label>
-      <input className="form-control m-2"></input>
+      <input
+        className="form-control m-2"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      ></input>
       <button className="btn btn-primary m-2">Sign Up</button>
     </form>
   );
