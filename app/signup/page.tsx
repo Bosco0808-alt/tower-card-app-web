a3bc2c766ee022cfd0dd64d0da247ab2b7ef11b5
bosco0808 <bosco0808@gmail.com>
2025-04-04 12:50:53 +0800

    Finished signup page Added firebase 🔥

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,3 +1,17 @@
 export default function SignUp() {
-  return <></>;
+  return (
+    <form>
+      <h2 className="m-2">Sign Up</h2>
+      <label className="m-2">Email address</label>
+      <input className="form-control m-2"></input>
+      <small className="form-text text-muted m-2">
+        We'll never share your email with anyone else.
+      </small>
+      <br />
+      <br />
+      <label className="m-2">Password</label>
+      <input className="form-control m-2"></input>
+      <button className="btn btn-primary m-2">Sign Up</button>
+    </form>
+  );
 }
