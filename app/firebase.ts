a7fe819cd06af46c5b54283d2e4cda58f5a51afb
bosco0808 <bosco0808@gmail.com>
2025-04-07 18:37:48 +0800

    emulators are now only enabled when during development and added sweetalert2 for better user expereince 🚀

--- app/firebase.ts
+++ app/firebase.ts
@@ -23,5 +23,7 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-connectAuthEmulator(auth, "http://127.0.0.1:9099");
-connectFirestoreEmulator(db, "127.0.0.1", 8080);
+if (process.env.NODE_ENV === "development") {
+  connectAuthEmulator(auth, "http://127.0.0.1:9099");
+  connectFirestoreEmulator(db, "127.0.0.1", 8080);
+}
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,6 +1,98 @@
-import SignUpClient from "./SignupClientComponent";
+"use client";
+
 import { createUser } from "../actions";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Swal from "sweetalert2";
 
 export default function SignUp() {
-  return <SignUpClient createUserAction={createUser} />;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      Swal.fire({
+        title: "Error!",
+        text: "Fill in both the email and the password fields!",
+        icon: "error",
+        confirmButtonText: "Ok",
+        timer: 3000,
+      });
+      return;
+    }
+
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailPattern.test(email)) {
+      Swal.fire({
+        title: "Error!",
+        text: "Invalid email!",
+        icon: "error",
+        confirmButtonText: "Ok",
+        timer: 3000,
+      });
+      return;
+    }
+
+    if (password.length < 6) {
+      Swal.fire({
+        title: "Error!",
+        text: "Password length must be over 6 characters long!",
+        icon: "error",
+        confirmButtonText: "Ok",
+        timer: 3000,
+      });
+      return;
+    }
+
+    const actionOut = await createUser(email, password); // Call the server action
+    const user = JSON.parse(actionOut);
+    if (JSON.stringify(user) === "{}") {
+      // 👆 check if there is an error
+      Swal.fire({
+        title: "Error!",
+        html: "<p>There was an error creating the user, please try again later.</p><p>Hint: have you already created a user under this email?</p>",
+        icon: "error",
+        confirmButtonText: "Ok",
+        timer: 6000,
+      });
+    } else {
+      Swal.fire({
+        title: "Success!",
+        text: "Signed up successfully!",
+        icon: "success",
+        confirmButtonText: "Ok",
+        timer: 3000,
+      });
+      router.push("/game");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="m-2">Sign Up</h2>
+      <label className="m-2">Email address</label>
+      <input
+        className="form-control m-2"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <small className="form-text text-muted m-2">
+        We'll never share your email with anyone else.
+      </small>
+      <br />
+      <label className="m-2">Password</label>
+      <input
+        className="form-control m-2"
+        value={password}
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button className="btn btn-primary m-2" type="submit">
+        Sign Up
+      </button>
+    </form>
+  );
 }
