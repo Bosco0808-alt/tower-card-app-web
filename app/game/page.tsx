9702b9c83a425dc367a76858366949bd2c61d084
bosco0808 <bosco0808@gmail.com>
2025-04-12 16:03:19 +0800

    Added /game home page

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,16 +1,27 @@
 "use client";
 
 import { useAuth } from "@/contexts/authContext";
+import Link from "next/link";
 
 export default function Game() {
   const { currentUser } = useAuth();
   return (
-    <span className="h2 m-2">
-      {currentUser?.displayName
-        ? currentUser?.displayName
-        : currentUser?.email
-        ? currentUser?.email
-        : "Guest"}
-    </span>
+    <>
+      <span className="h2 m-2">
+        {currentUser?.displayName
+          ? `Hello ${currentUser?.displayName}`
+          : currentUser?.email
+          ? `Hello ${currentUser?.email}`
+          : "Hello guest"}
+      </span>
+      <br />
+      <Link href="#" className="btn btn-primary m-2">
+        Start PvP game
+      </Link>
+      <br />
+      <Link href="#" className="btn btn-secondary m-2">
+        Start PvE game
+      </Link>
+    </>
   );
 }
